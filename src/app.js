013751086ba2a5d068d71043257e168d55f82376
Kartik <9415kks@gmail.com>
2021-10-01 13:30:13 +0530

    Added the update feature

--- src/app.js
+++ src/app.js
@@ -7,20 +7,63 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // create a new students
-app.post("/students",(req,res) =>{
+app.post("/students", (req, res) => {
 
   console.log(req.body);
 
   const user = new Student(req.body);
-  user.save().then(() =>{
+  user.save().then(() => {
     res.send(user);
-  }).catch((e) =>{
+  }).catch((e) => {
     res.send(e);
   });
 
   // res.send("Hello Kartik");
 })
 
-app.listen(port, ()=>{
+app.get("/students", async (req, res) => {
+
+  try {
+    const studentsData = await Student.find();
+    res.send(studentsData);
+  } catch (e) {
+    res.send(e);
+  }
+})
+
+app.get('/students/:id', async (req, res) => {
+  try {
+    const _id = req.params.id;
+    // console.log(_id.id);
+    // res.send(req.params.id);
+
+    const studentData = await Student.findById(_id);
+
+    if (!studentData) {
+      return res.status(404).send();
+    } else {
+      res.send(studentData);
+    }
+
+  } catch (e) {
+    res.status(500).send(e);
+  }
+})
+
+// update the students by it's id
+
+app.patch("/students/:id",async (req,res) =>{
+  try{
+    const _id = req.params.id;
+    const updateStudents = await Student.findByIdAndUpdate(_id,req.body,{
+      new:true
+    });
+    res.send(updateStudents);
+  }catch(e){
+    res.status(404).send(e);
+  }
+})
+
+app.listen(port, () => {
   console.log(`connection is setup at ${port}`);
 })
